04d61ddce0979a360ad412b4d82add2aca147849
Ben Ashby <mail@benashby.com>
2012-03-21 11:05:53 -0600

    enabled gii

--- www_root/protected/config/main.php
+++ www_root/protected/config/main.php
@@ -19,15 +19,11 @@ return array(
 	),
 
 	'modules'=>array(
-		// uncomment the following to enable the Gii tool
-		/*
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
 			'password'=>'Enter Your Password Here',
-		 	// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
-		*/
 	),
 
 	// application components
